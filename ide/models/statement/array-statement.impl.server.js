cd97828a74cb0d78cceb184889b822ae0ade459d
manasa-konidena <manasa.v.konidena@gmail.com>
2016-07-30 13:35:04 -0400

    array functions half way through.

--- ide/models/statement/array-statement.impl.server.js
+++ ide/models/statement/array-statement.impl.server.js
@@ -6,28 +6,74 @@ module.exports = function () {
 
     function execute(statement, model) {
 
-        var array1, array2;
+        var arg1, arg2;
         if(statement.input[0]) {
-            array1 = model[statement.input[0].variable];
-            if(typeof array1 === 'undefined') {
-                array1 = statement.input[0].literal;
+            arg1 = model[statement.input[0].variable];
+            if(typeof arg1 === 'undefined') {
+                arg1 = statement.input[0].literal;
             }
         }
         if(statement.input[1]) {
-            array2 = model[statement.input[1].variable];
-            if(typeof array2 === 'undefined') {
-                array2 = statement.input[1].literal;
+            arg2 = model[statement.input[1].variable];
+            if(typeof arg2 === 'undefined') {
+                arg2 = statement.input[1].literal;
             }
         }
 
         switch (statement.operation) {
             case 'CONCAT' :
-                model[statement.output] = array1.concat(array2);
+                model[statement.output] = arg1.concat(arg2);
                 break;
             case 'EVERY' :
-                model[statement.output] = array1.every(function (element) {
-                    return element >= 90;
+                model[statement.output] = arg1.every(function (element) {
+                    return element >= arg2;
                 });
+                break;
+            case 'FILL':
+                model[statement.output] = arg1.fill(arg2);
+                break;
+            case 'FILTER' :
+                model[statement.output] = arg1.filter(function (element) {
+                    return element >= arg2;
+                });
+                break;
+            case 'FIND' :
+                model[statement.output] = arg1.find(function (element) {
+                    return element === arg2;
+                });
+                break;
+            case 'FINDINDEX' :
+                model[statement.output] = arg1.findIndex(function (element) {
+                    return element === arg2;
+                });
+                break;
+            case 'FOREACH' :
+                var sum = 0;
+                arg1.forEach(function (element) {
+                    sum += element;
+                });
+                model[statement.output] = sum;
+                break;
+            case 'INDEXOF' :
+                model[statement.output] = arg1.indexOf(arg2);
+                break;
+            case 'ISARRAY' :
+                model[statement.output] = Array.isArray(arg1);
+                break;
+            case 'JOIN' :
+                model[statement.output] = arg1.join();
+                break;
+            case 'LASTINDEXOF' :
+                model[statement.output] = arg1.lastIndexOf(arg2);
+                break;
+            case 'MAP' :
+                model[statement.output] = arg1.map(function (element) {
+                    var temp = element + arg2 + "";
+                    return temp;
+                });
+                break;
+            case 'POP' :
+                model[statement.output] = arg1.pop();
                 break;
         }
         return null;
