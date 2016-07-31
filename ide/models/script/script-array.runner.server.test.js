942efaadce632e332213bd955a5c65717e86dbe7
manasa-konidena <manasa.v.konidena@gmail.com>
2016-07-30 20:02:04 -0400

    All array functions implemented and tested.

--- ide/models/script/script-array.runner.server.test.js
+++ ide/models/script/script-array.runner.server.test.js
@@ -214,23 +214,242 @@ var statements = [
     },
     {
         type: 'ARRAY',
-        operation: 'FILL',
+        operation: 'POP',
         input:[
-            {variable:'A'},
-            {literal: 'fill in'}
+            {variable:'B'}
         ],
         output: 'C'
     },
-    { // The array A with all elements replaced by "fill in"
+    { // The last element of the array is
         type: 'CONSOLE',
         operation: 'LOG',
-        input:[{literal: 'The array A with all elements replaced by "fill in"'}]
+        input:[{literal: 'The last element of the array is'}]
     },
-    {   // Displays the array with the same element. After the fill function
+    {   // Displays the string after joining
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'PUSH',
+        input:[
+            {variable:'B'},
+            {literal: "MEANStack"}
+        ],
+        output: 'C'
+    },
+    { // THe length of the newly formed array is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'THe length of the newly formed array is'}]
+    },
+    {   // Displays the string after joining
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'REDUCE',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // THe reduced value of the array is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'THe reduced value of the array is'}]
+    },
+    {   // Displays the string after joining
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'REDUCERIGHT',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // THe reduced value of the array from the right is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'THe reduced value of the array from the right is'}]
+    },
+    {   // Displays the string after joining
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'SHIFT',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // The extracted first element from the array is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'The extracted first element from the array is'}]
+    },
+    {   // Displays the element
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'SLICE',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // The sliced array is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'The sliced array is'}]
+    },
+    {   // Displays the sliced array
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'SOME',
+        input:[
+            {variable:'B'},
+            {literal: 30}
+        ],
+        output: 'C'
+    },
+    { // True if any of the elementsin the array are less than 30
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'True if any of the elementsin the array are less than 30'}]
+    },
+    {
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'SORT',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // The array after sorting is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'The array after sorting is'}]
+    },
+    {
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'SPLICE',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // The array after splicing in is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'The array after splicing in is'}]
+    },
+    {
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'B'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'TOSTRING',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // The array after converting to string is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'The array after converting to string is'}]
+    },
+    {
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'UNSHIFT',
+        input:[
+            {variable:'B'},
+            {literal: 'application'}
+        ],
+        output: 'C'
+    },
+    { // The array after unshifting "application" is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'The array after unshifting "application" is'}]
+    },
+    {
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{variable: 'C'}]
+    },
+    {
+        type: 'ARRAY',
+        operation: 'VALUEOF',
+        input:[
+            {variable:'B'}
+        ],
+        output: 'C'
+    },
+    { // The primitive value of array B is
+        type: 'CONSOLE',
+        operation: 'LOG',
+        input:[{literal: 'The primitive value of array B is'}]
+    },
+    {
         type: 'CONSOLE',
         operation: 'LOG',
         input:[{variable: 'C'}]
     }
+    // {
+    //     type: 'ARRAY',
+    //     operation: 'FILL',
+    //     input:[
+    //         {variable:'A'},
+    //         {literal: 'fill in'}
+    //     ],
+    //     output: 'C'
+    // },
+    // { // The array A with all elements replaced by "fill in"
+    //     type: 'CONSOLE',
+    //     operation: 'LOG',
+    //     input:[{literal: 'The array A with all elements replaced by "fill in"'}]
+    // },
+    // {   // Displays the array with the same element. After the fill function
+    //     type: 'CONSOLE',
+    //     operation: 'LOG',
+    //     input:[{variable: 'C'}]
+    // }
 ];
 
 var script = {
@@ -238,8 +457,8 @@ var script = {
 };
 
 var model = {
-    A: [90, 92, 89],
-    B: ["web", "development", "ftw"]
+    A: [103, 56, 67, 90, 92, 89],
+    B: ["remove", "web", "development", "ftw"]
 
 };
 
