ed46c24aac87a1ad5f32fd89109eeb9168478525
sruthinagalla <sruthi1991n@gmail.com>
2016-08-26 21:11:30 +0530

    Added object type for input of operands in number statement

--- ide/models/statement/number-statement.schema.server.js
+++ ide/models/statement/number-statement.schema.server.js
@@ -6,8 +6,8 @@ module.exports = function() {
         operation : {type : String, enum : ['ADDITION', 'SUBTRACTION', 'MULTIPLICATION', 'DIVISION', 'GREATER_THAN',
             'GREATER_THAN_EQUAL_TO', 'LESSER_THAN', 'LESSER_THAN_EQUAL_TO', 'EQUAL_TO', 'POWER', 'LOGARITHM', 'ROUND',
             'ABSOLUTE', 'EXPONENTIAL', 'COS', 'SIN', 'TAN', 'CEIL', 'FLOOR', 'SQRT', 'VARIABLE']},
-        operand1        : String,
-        operand2        : String
+        operand1        : require("./input-statement.schema.server")(),
+        operand2        : require("./input-statement.schema.server")()
     });
 
     return NumberStatementSchema;
